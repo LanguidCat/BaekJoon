54a519969c12dd25518c26dcf2766fa72964de10
LeeJunhee <45219806+LanguidCat@users.noreply.github.com>
2020-08-19 14:02:23 +0900

    Create 2576.c

---
+++ 2576 - 홀수/2576.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int sum = 0, min = 100; // 최솟값 초기 100으로 설정
+	  for(int i = 0; i < 7; i++){
+		  int tmp; scanf("%d", &tmp);
+		  if(tmp%2==1){ // 홀수일 경우
+			    sum += tmp;
+			    if(tmp<min){ // 최솟값 갱신
+				  min = tmp;
+			    }
+		  }
+    }
+    if(sum==0 && min==100) printf("%d", -1);
+	  else printf("%d \n%d", sum, min);
+}
