b177e22373bc1f9544582e8b58db6b81b1a5a029
LeeJunhee <45219806+LanguidCat@users.noreply.github.com>
2020-08-27 00:02:43 +0900

    Create 2475.c

---
+++ 2475 - 검증수/2475.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	int arr[5] = {}, sum = 0;
+	scanf("%d %d %d %d %d", &arr[0], &arr[1], &arr[2], &arr[3], &arr[4]);
+	for(int i=0; i<5; i++){
+		sum += arr[i]*arr[i];
+	}
+	printf("%d", sum%10);
+}
