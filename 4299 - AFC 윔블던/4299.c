f62ad0cf1d4468e2749ec01419ed53f0283cc110
LeeJunhee <45219806+LanguidCat@users.noreply.github.com>
2020-08-31 20:26:23 +0900

    Update 4299.c

--- 4299 - AFC 윔블던/4299.c
+++ 4299 - AFC 윔블던/4299.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 
 int main(){
-	int sum, dif;
-	scanf("%d %d", &sum, &dif);
-	for(int i=0; i<1001; i++){
-		for(int j=0; j<1001; j++){
-			if(i+j == sum){
-				if(i-j == dif){
-					printf("%d %d", i, j);
-					return 0;
-				}
-				else if(j-i == dif){
-					printf("%d %d", j, i);
-					return 0;
-				}
-			}
+    int sum, dif;
+    scanf("%d %d", &sum, &dif);
+    for(int i=0; i<1001; i++){
+        for(int j=0; j<1001; j++){
+	    if(i+j == sum){
+                if(i-j == dif){
+		    printf("%d %d", i, j);
+		    return 0;
 		}
-	}
+	        else if(j-i == dif){
+		    printf("%d %d", j, i);
+		    return 0;
+		}
+	    }
+    }
 	printf("%d", -1);
-}
