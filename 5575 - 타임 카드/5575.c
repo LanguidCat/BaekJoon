fd17a23b1dd4fed2d234990b8f76cdd72a0525b1
LeeJunhee <45219806+LanguidCat@users.noreply.github.com>
2020-09-01 23:37:24 +0900

    Update 5575.c

--- 5575 - 타임 카드/5575.c
+++ 5575 - 타임 카드/5575.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 int main(){
-	for(int i=0; i<3; i++){
-		int h, m, s, H, M, S, rh, rm, rs;
-		scanf("%d %d %d %d %d %d", &h, &m, &s, &H, &M, &S);
-		rh = H-h;
-		if(M-m<0){
-			rh -= 1;
-			rm = 60-(m-M);
-		}
-		else rm = M-m;
-		if(S-s<0){
-			rm -= 1;
-			if(rm<0){
-				rh -= 1;
-				rm = 60+rm; // 0분 0초에서 초 단위로 줄어들 경우 
-			}
-			rs = 60-(s-S);
-		}
-		else rs = S-s;
-		printf("%d %d %d \n", rh, rm, rs);
+    for(int i=0; i<3; i++){
+        int h, m, s, H, M, S, rh, rm, rs;
+        scanf("%d %d %d %d %d %d", &h, &m, &s, &H, &M, &S);
+	rh = H-h;
+	if(M-m<0){
+	    rh -= 1;
+	    rm = 60-(m-M);
 	}
+	else rm = M-m;
+	if(S-s<0){
+	    rm -= 1;
+	    if(rm<0){
+                rh -= 1;
+		rm = 60+rm; // 0분 0초에서 초 단위로 줄어들 경우 
+	    }
+	    rs = 60-(s-S);
+	}
+	else rs = S-s;
+	printf("%d %d %d \n", rh, rm, rs);
+    }
 }
