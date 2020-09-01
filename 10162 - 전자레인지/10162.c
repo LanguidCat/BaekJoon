7847a45fb5ecc7b2d148f9a877e7665c23f1a934
LeeJunhee <45219806+LanguidCat@users.noreply.github.com>
2020-09-02 02:49:50 +0900

    Update 10162.c

--- 10162 - 전자레인지/10162.c
+++ 10162 - 전자레인지/10162.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 int main(){
-	int T, A = 0, B = 0, C = 0; 
-	scanf("%d", &T);
-	if(T%10!=0){
-		printf("%d", -1);
-		return 0;
-	} 
-	while(T>=300){
-		T -= 300;
-		A++;
-	}
-	while(T>=60){
-		T -= 60;
-		B++;
-	}
-	while(T>=10){
-		T -= 10;
-		C++;
-	}
-	printf("%d %d %d", A, B, C);
+    int T, A = 0, B = 0, C = 0; 
+    scanf("%d", &T);
+    if(T%10!=0){
+        printf("%d", -1);
+        return 0;
+    } 
+    while(T>=300){
+        T -= 300;
+        A++;
+    }
+    while(T>=60){
+        T -= 60;
+        B++;
+    }
+    while(T>=10){
+    T -= 10;
+    C++;
+    }
+    printf("%d %d %d", A, B, C);
 }
