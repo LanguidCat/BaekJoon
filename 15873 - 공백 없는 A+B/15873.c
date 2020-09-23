5e5320ea5abcbd29ecfbbf69f561d7b469ed1594
LeeJunhee <45219806+LanguidCat@users.noreply.github.com>
2020-09-23 18:27:00 +0900

    Create 15873.c

---
+++ 15873 - 공백 없는 A+B/15873.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    char arr[5];
+    scanf("%s", arr);
+    int num=0;
+    for(int i=0; arr[i] != '\0'; i++) num++;
+    // 자릿수를 기준으로 판단한다  
+    if(num == 2){
+        int temp=arr[0] - '0' + arr[1] - '0';
+        printf("%d", temp);
+    }
+    else if(num == 3){
+        if(arr[1] == '0'){
+            int temp=(arr[0] - '0') * 10 + arr[2] - '0';
+            printf("%d", temp);
+		    }
+		    else{
+            int temp=arr[0] - '0' + (arr[1] - '0') * 10;
+		        printf("%d", temp);
+		    }
+    }
+    else printf("%d", 20); 
+}
