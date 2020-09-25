eea6d056a2bce48f01f3718222e2e907b21202a1
LeeJunhee <45219806+LanguidCat@users.noreply.github.com>
2020-09-25 17:40:06 +0900

    Update 2743.c

--- 2743 - 단어 길이 재기/2743.c
+++ 2743 - 단어 길이 재기/2743.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main(){
-	char str[101] = {}; scanf("%s", str);
-	int idx=0;
-	for(int i=0; str[i]!=0; i++){
-		int tmp;
-		tmp = str[i];
-		idx++;
-	}
-	printf("%d", idx);
+    char str[101] = {}; scanf("%s", str);
+    int idx=0;
+    for(int i=0; str[i]!=0; i++){
+        int tmp;
+	tmp = str[i];
+	idx++;
+    }
+    printf("%d", idx);
 }
