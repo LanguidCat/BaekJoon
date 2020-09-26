62cd37ce1b360ca2e30b8aa9be8942066c789253
LeeJunhee <45219806+LanguidCat@users.noreply.github.com>
2020-09-26 20:59:59 +0900

    Create 10174.cpp

---
+++ 10174 - 팰린드롬/10174.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <algorithm> 
+#define endl '\n'
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    cin.ignore();
+    for(int i = 0; i < n; i++){
+        string s, revs;
+		    getline(cin, s);
+		    for(int j = 0; j < s.size(); j++){
+            if(s[j] <= 'Z') s[j] += 32; // 대문자를 소문자로 고친다 
+		    }
+		    revs = s;
+		    reverse(revs.begin(), revs.end());
+		    if(s == revs) cout << "Yes" << endl;
+		    else cout << "No" << endl;
+    } 
+}
