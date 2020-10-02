df10cfbb2fe102eac1798315383be3fc801ffe1f
LeeJunhee <45219806+LanguidCat@users.noreply.github.com>
2020-10-02 21:55:15 +0900

    Create 11931.cpp

---
+++ 11931 - 수 정렬하기 4/11931.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#define endl '\n'
+using namespace std;
+
+int main(){
+    vector<int> v;
+	int N;
+	cin >> N;
+    for(int i = 0; i < N; i++){
+    	int temp;
+    	cin >> temp;
+    	v.push_back(temp);
+	}
+	sort(v.begin(), v.end());
+	reverse(v.begin(), v.end());
+	for(int i = 0; i < N; i++){
+		cout << v[i] << endl;
+	}
+}
