97caa810e42e3a2b0672f80aa8eb702b10a16974
LeeJunhee <45219806+LanguidCat@users.noreply.github.com>
2020-10-02 20:54:04 +0900

    Update 2751.cpp

--- 2751 - 수 정렬하기 2/2751.cpp
+++ 2751 - 수 정렬하기 2/2751.cpp
@@ -6,15 +6,15 @@ using namespace std;
 
 int main(){
     vector<int> v;
-	int N;
-	cin >> N;
+    int N;
+    cin >> N;
     for(int i = 0; i < N; i++){
     	int temp;
     	cin >> temp;
     	v.push_back(temp);
-	}
-	sort(v.begin(), v.end());
-	for(int i = 0; i < N; i++){
-		cout << v[i] << endl;
-	}
+    }
+    sort(v.begin(), v.end());
+    for(int i = 0; i < N; i++){
+        cout << v[i] << endl;
+    }
 }
