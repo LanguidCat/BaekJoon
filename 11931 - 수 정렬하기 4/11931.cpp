87e7f8cd8997f9f31d19a5348fe9f13272c09d9f
LeeJunhee <45219806+LanguidCat@users.noreply.github.com>
2020-10-02 21:55:48 +0900

    Update 11931.cpp

--- 11931 - 수 정렬하기 4/11931.cpp
+++ 11931 - 수 정렬하기 4/11931.cpp
@@ -6,16 +6,16 @@ using namespace std;
 
 int main(){
     vector<int> v;
-	int N;
-	cin >> N;
+    int N;
+    cin >> N;
     for(int i = 0; i < N; i++){
     	int temp;
     	cin >> temp;
     	v.push_back(temp);
-	}
-	sort(v.begin(), v.end());
-	reverse(v.begin(), v.end());
-	for(int i = 0; i < N; i++){
-		cout << v[i] << endl;
-	}
+    }
+    sort(v.begin(), v.end());
+    reverse(v.begin(), v.end());
+    for(int i = 0; i < N; i++){
+        cout << v[i] << endl;
+    }
 }
