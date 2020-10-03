80f47425a95825cb1f9763bfc13868661ad7a8a6
LeeJunhee <45219806+LanguidCat@users.noreply.github.com>
2020-10-03 23:46:35 +0900

    Update 15921.cpp

--- 15921 - 수찬은 마린보이야!!/15921.cpp
+++ 15921 - 수찬은 마린보이야!!/15921.cpp
@@ -3,30 +3,29 @@
 using namespace std;
 
 int main(){
-	vector <int> v;
-	double N, average = 0, sum = 0, expect = 0;
-	cin >> N;
-	if(N==0){
-		cout << "divide by zero";
-		return 0;
-	}
-	for(int i = 0; i < N; i++){
-		int temp;
-		cin >> temp;
-		sum += temp;
-		v.push_back(temp);
-	}
-	average = sum / N;
+    vector <int> v;
+    double N, average = 0, sum = 0, expect = 0;
+    cin >> N;
+    if(N==0){
+        cout << "divide by zero";
+        return 0;
+    }
+    for(int i = 0; i < N; i++){
+        int temp;
+	cin >> temp;
+	sum += temp;
+	v.push_back(temp);
+    }
+    average = sum / N;
 	
-	// 기댓값 산출 
-	for(int i = 0; i < N; i++){
-		expect += v[i] / N;
-	}
-
-	if(expect == 0){
-		cout << "divide by zero";
-		return 0;
-	}
-	// 출력 
-	else printf("%.2f", average / expect);
+    // 기댓값 산출 
+    for(int i = 0; i < N; i++){
+        expect += v[i] / N;
+    }
+    if(expect == 0){
+        cout << "divide by zero";
+        return 0;
+    }
+    // 출력 
+    else printf("%.2f", average / expect);
 }
