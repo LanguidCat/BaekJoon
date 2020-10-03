1413dae69576c5fc26542aef25eb43ff5786b438
LeeJunhee <45219806+LanguidCat@users.noreply.github.com>
2020-10-03 12:39:08 +0900

    Update 4344.cpp

--- 4344 - 평균은 넘겠지/4344.cpp
+++ 4344 - 평균은 넘겠지/4344.cpp
@@ -12,20 +12,20 @@ int main(){
     	vector <int> v;
     	cin >> N;
     	
-		// 성적 입력받아 합을 구하고 v에 저장  
-		for(int j = 0; j < N; j++){
-    		int score;
-    		cin >> score;
-    		sum += score;
-    		v.push_back(score);
-		}
-		
-		// 평균 구하고 학생 비율 출력 
-		average = sum / N;
-		for(int j = 0; j < N; j++){
-			if(v[j] > average) num++;
-		}
-		result = (double)num / N * 100;
-		printf("%.3f%% \n", result);
+        // 성적 입력받아 합을 구하고 v에 저장  
+	for(int j = 0; j < N; j++){
+    	    int score;
+    	    cin >> score;
+            sum += score;
+    	    v.push_back(score);
 	}
+		
+	// 평균 구하고 학생 비율 출력 
+	average = sum / N;
+	for(int j = 0; j < N; j++){
+	    if(v[j] > average) num++;
+	}
+	result = (double)num / N * 100;
+	printf("%.3f%% \n", result);
+    }
 }
