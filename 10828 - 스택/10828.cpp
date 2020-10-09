23a2ea3f0e332d77f6b7ffb2781ea7c832cf7a57
LeeJunhee <45219806+LanguidCat@users.noreply.github.com>
2020-10-09 18:30:55 +0900

    Update 10828.cpp

--- 10828 - 스택/10828.cpp
+++ 10828 - 스택/10828.cpp
@@ -6,51 +6,50 @@ using namespace std;
 
 int main(){
     // 프로그램의 효율성을 위함
-	cin.tie(NULL);
-	ios_base::sync_with_stdio(false);
-	
-	int N;
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N;
 	cin >> N;
 	
 	stack <int> stk;
 	
-	
 	for(int i = 0; i < N; i++){
-		string s;
-		cin >> s;
+	    string s;
+	    cin >> s;
+	    
+	    if(s == "push"){
+	        int num;
+		    cin >> num;
+		    stk.push(num);
+	    }
 		
-		if(s == "push"){
-			int num;
-			cin >> num;
-			stk.push(num);
-		}
+	    else if(s == "pop"){
+		    if(stk.empty()){
+		    cout << "-1" << endl;
+	        }
+		    else{
+		        cout << stk.top() << endl;
+		        stk.pop();
+		    }
+	    }
 		
-		else if(s == "pop"){
-			if(stk.empty()){
-				cout << "-1" << endl;
-			}
-			else{
-				cout << stk.top() << endl;
-				stk.pop();
-			}
-		}
+	    else if(s == "size"){
+		    cout << stk.size() << endl;
+	    }
 		
-		else if(s == "size"){
-			cout << stk.size() << endl;
-		}
+	    else if(s == "empty"){
+	        cout << stk.empty() << endl;
+	    }
 		
-		else if(s == "empty"){
-			cout << stk.empty() << endl;
+	    else if(s == "top"){
+	        if(stk.empty()){
+		    cout << "-1" << endl;
 		}
-		
-		else if(s == "top"){
-			if(stk.empty()){
-				cout << "-1" << endl;
-			}
-			else{
-				cout << stk.top() << endl;
-			}
-		}
+		    else{
+		        cout << stk.top() << endl;
+		    }
+	    }
 	}
 	return 0;
 }
