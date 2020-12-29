8df3e05ef4d6ed0a8f0e7a2341dba60fced3a7f1
LeeJunhee <45219806+LanguidCat@users.noreply.github.com>
2020-12-30 01:03:10 +0900

    Create 17009.py

---
+++ 17009 - Winning Score/17009.py
@@ -0,0 +1,12 @@
+A = 0
+A += int(input()) * 3
+A += int(input()) * 2
+A += int(input()) * 1
+B = 0
+B += int(input()) * 3
+B += int(input()) * 2
+B += int(input()) * 1
+
+if(A > B): print('A')
+elif(B > A): print('B')
+else: print('T')
