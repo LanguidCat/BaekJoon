58b02883ea8ab1fb96976f63968126acae77a922
LeeJunhee <45219806+LanguidCat@users.noreply.github.com>
2021-01-13 12:35:51 +0900

    Create 20540.py

---
+++ 20540 - 연길이의 이상형/20540.py
@@ -0,0 +1,11 @@
+STR = input()
+
+for i in STR:
+    if i == 'I': print('E', end = "")
+    elif i == 'E': print('I', end = "")
+    elif i == 'S': print('N', end = "")
+    elif i == 'N': print('S', end = "")
+    elif i == 'T': print('F', end = "")
+    elif i == 'F': print('T', end = "")
+    elif i == 'J': print('P', end = "")
+    elif i == 'P': print('J', end = "")
