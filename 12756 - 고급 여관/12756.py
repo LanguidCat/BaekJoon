d634e67980db8f409905d198e4a4ec068d2bd7e3
LeeJunhee <45219806+LanguidCat@users.noreply.github.com>
2021-01-17 23:04:40 +0900

    Create 12756.py

---
+++ 12756 - 고급 여관/12756.py
@@ -0,0 +1,11 @@
+import sys
+
+a1, a2 = map(int, sys.stdin.readline().split())
+b1, b2 = map(int, sys.stdin.readline().split())
+
+while a2 > 0 and b2 > 0:
+    a2 -= b1
+    b2 -= a1
+    if a2 <= 0 and b2 <=0: print("DRAW")
+    elif a2 <= 0: print("PLAYER B")
+    elif b2 <= 0: print("PLAYER A")
