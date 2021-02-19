cfc48a9e9eaefa3ce07c77f9909c48f777b0b83a
LeeJunhee <45219806+LanguidCat@users.noreply.github.com>
2021-02-19 10:50:33 +0900

    Update 2839.cpp

--- 2839 -  설탕 배달/2839.cpp
+++ 2839 -  설탕 배달/2839.cpp
@@ -4,54 +4,28 @@ using namespace std;
 int main(){
     cin.tie(0); ios::sync_with_stdio(0);
     int n; cin >> n;
-    int a = 9999, b = 0, c = 0, tmp = n;
+    int cnt = 0;
 
-    if (n % 3 == 0){
-        a = n / 3;
-    }
-    
-    if (n % 5 == 0){
-        a = n / 5;
-    }
-
-    if (n < 5){
-        b = 9999;
-    }
-    else{
-        while (tmp >= 1){
-            tmp -= 5;
-            b++;
-            
-            if (tmp % 3 == 0){
-                b += tmp / 3;
-                break;
-            }
+    while (n > 0){
+        if (n % 5 == 0){
+            n -= 5;
+            cnt++;
         }
-
-        if (tmp < 0)
-            b = 9999;
-    }
-
-    tmp = n;
-    while (tmp >= 1){
-        tmp -= 3;
-        c++;
-
-        if (tmp % 5 == 0){
-            c += tmp / 5;
+        else if (n % 3 == 0){
+            n -= 3;
+            cnt++;
+        }
+        else if (n > 5){
+            n -= 5;
+            cnt++;
+        }
+        else{
+            cnt = -1;
             break;
         }
     }
 
-    if (tmp < 0)
-        c = 9999;
-
-    if (a == 9999 && b == 9999 && c == 9999){
-        cout << -1 << '\n';
-    }
-    else{
-        cout << min({a, b, c}) << '\n';
-    }
+    cout << cnt << '\n';
 
     return 0;
 }
